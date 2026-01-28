dbb8d5e9a2ce27ba0b377fc391a56518ae89f4f8
pmeaney <patrick.wm.meaney@gmail.com>
2026-01-28 12:45:12 -0600

    forgot backslash

--- my-shop-juniper/apps/server/src/index.ts
+++ my-shop-juniper/apps/server/src/index.ts
@@ -1,6 +1,6 @@
 import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
-
+// test
 runMigrations(config)
     .then(() => bootstrap(config))
     .catch(err => {
