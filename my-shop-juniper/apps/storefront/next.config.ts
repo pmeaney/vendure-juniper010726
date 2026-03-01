d5c1416852e0adc4c1dc3fd7db3ecd0a5ebfb7dc
pmeaney <patrick.wm.meaney@gmail.com>
2026-03-01 16:49:23 -0600

    had to add api.website.com site as remotePatterns to next.config.ts

--- my-shop-juniper/apps/storefront/next.config.ts
+++ my-shop-juniper/apps/storefront/next.config.ts
@@ -1,28 +1,33 @@
-import {NextConfig} from 'next';
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    cacheComponents: true,
-    images: {
-        // This is necessary to display images from your local Vendure instance
-        dangerouslyAllowLocalIP: true, // <-- needed for NextJS image optimization to not be buggy during local dev work
-        remotePatterns: [
-            {
-              protocol: 'http',
-              hostname: 'vendure-server',
-              port: '3000',
-              pathname: '/assets/**',
-            },
-            {
-              protocol: 'http',
-              hostname: 'localhost',
-              port: '3000',
-              pathname: '/assets/**',
-            },
-        ],
-    },
-    experimental: {
-        rootParams: true
-    }
+  cacheComponents: true,
+  images: {
+    // This is necessary to display images from your local Vendure instance
+    dangerouslyAllowLocalIP: true, // <-- needed for NextJS image optimization to not be buggy during local dev work
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "vendure-server",
+        port: "3000",
+        pathname: "/assets/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/assets/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.vivaluthiers.com",
+        pathname: "/assets/**",
+      },
+    ],
+  },
+  experimental: {
+    rootParams: true,
+  },
 };
 
 export default nextConfig;
