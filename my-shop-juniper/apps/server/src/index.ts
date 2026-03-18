16765c7f911606d743bddb36ca825f60a712e7f5
pmeaney <patrick.wm.meaney@gmail.com>
2026-03-18 13:28:02 -0500

    adding trust proxy

--- my-shop-juniper/apps/server/src/index.ts
+++ my-shop-juniper/apps/server/src/index.ts
@@ -3,6 +3,9 @@ import { config } from "./vendure-config";
 
 runMigrations(config)
   .then(() => bootstrap(config))
+  .then((app) => {
+    app.getHttpAdapter().getInstance().set("trust proxy", 1);
+  })
   .catch((err) => {
     console.log(err);
   });
